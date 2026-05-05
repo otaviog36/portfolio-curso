8351462edd1827b12a2bdb8a697a4d74a3aa9732
otaviog36 <74985404+otaviog36@users.noreply.github.com>
2026-05-05 00:01:19 +0000

    Commit-Date Object-04/05/26

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -1,26 +1,50 @@
-
+// Dados Pessoais-----------------------------------------------------------------------------------------------------
 const NOME = "Otávio Gongra";
 let tituloProfissional = "otaviog36";
 let minhaBio = "Estudante de ADS na proz de Divinópolis - MG";
 let anoFormatura = 2026;
+let mesFormatura = 12;
+let diaFormatura = 30;
 let anoIngresso = 2025;
+let mesIngresso = 1;
+let diaIngresso = 1;
+//Utilização do Date--------------------------------------------------------------------------------------------------
+let dataAtual = new Date();
+let mesAtual = dataAtual.getMonth() + 1; // Começa por 0, por isso do +1
+let anoAtual = dataAtual.getFullYear();
+let diaAtual = dataAtual.getDate();
+let hoje = diaAtual + mesAtual + anoAtual;
+//--------------------------------------------------------------------------------------------------------------------
 let indefinido;
 let nulo = null;
 let curso = {
     nome: "Análise e desenvolvimento de sistemas",
     semestre: 3,
     disciplinaAtual: "Linguagem de programação"
-}
+};
 
-console.log(typeof nulo);
-console.log(typeof indefinido);
 console.log(typeof anoFormatura);
-console.log(typeof minhaBio);
-console.log(typeof tituloProfissional);
 console.log(typeof NOME);
+console.log(typeof tituloProfissional);
+console.log(typeof minhaBio);
+console.log(typeof anoIngresso);
+console.log(typeof indefinido);
+console.log(typeof nulo);
 console.log(typeof curso);
+//Calculo-------------------------------------------------------------------------------------------------------------
+let anoRestante = anoFormatura - anoAtual;
+let mesRestante = mesFormatura - mesAtual;
+let diaRestante = diaFormatura - diaAtual;
+//--------------------------------------------------------------------------------------------------------------------
+console.log(typeof anoRestante);
+console.log(typeof mesRestante);
+console.log(typeof diaRestante);
 
 document.getElementById("meuNome").innerText = NOME;
 document.getElementById("tituloProfissional").innerText = tituloProfissional;
 document.getElementById("minhaBio").innerText = minhaBio;
-document.getElementById("anoFormatura").innerText = "Ano de formatura:" + ganoFormatura;
+document.getElementById("anoFormatura").innerText = `Formatura prevista para: ${diaFormatura}/${mesFormatura}/${anoFormatura}`;
+document.getElementById("anoInfresso").innerText = `Ano de Ingresso: ${anoIngresso}`;
+document.getElementById("curso").innerText = `Curso: ${curso.nome} - Semestre: ${curso.semestre}`;
+document.getElementById("tempoRestanteParaFormatura").innerText = 
+`Tempo restante para a formatura: ${anoRestante} ano(s), ${mesRestante} meses e ${diaRestante} dias`;
